048b5057d536dde9ca46e43f616d02ff52080636
Nikita Korotkin <nickkoro02@gmail.com>
2020-07-21 20:30:49 +0200

    Someone's gonna have a rough day playing dat

--- scripts/buff-enemies.js
+++ scripts/buff-enemies.js
@@ -1,5 +1,39 @@
-print(UnitTypes.dagger.health)
+const allBuffs = { healthFactor: 1.5, speedFactor: 1.5 };
 
-UnitTypes.dagger.health = 1;
+const specificBuffs = {
+    dagger: { health: 260, speed: 0.3, maxVelocity: 1.5 },
+    crawler: { health: 200, speed: 10, maxVelocity: 5 },
+    eruptor: { health: 1300, speed: 0.2, maxVelocity: 0.5 },
+    lich: { health: 20000, speed: 0.01, maxVelocity: 0.5 },
+    eradicator: { health: 30000, speed: 0.1, maxVelocity: 0.5 },
+};
 
-print(UnitTypes.dagger.health)
+
+function getAllUnits(){
+    return Object.keys(UnitTypes).map(name => UnitTypes[name]);
+}
+
+function buffAllUnits(buff){
+    getAllUnits().forEach(unit => {
+        unit.health *= buff.healthFactor;
+        unit.speed *= buff.speedFactor;
+    });
+}
+
+function buffSpecific(name, buff){
+    const unit = UnitTypes[name];
+
+    Object.keys(buff).forEach( stat =>
+        unit[stat] = buff[stat]
+    );
+}
+
+function executeBuffs(){
+    buffAllUnits(allBuffs);
+
+    Object.keys(specificBuffs)
+        .forEach(e => buffSpecific(e, specificBuffs[e]));
+}
+
+
+executeBuffs()
